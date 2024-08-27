ab2e5c0f33a6ddd05dafd0ec2f9ff81eaa05f2a5
Henrique Souza <johanhenrique2k24@gmail.com>
2024-08-26 22:50:22 -0300

    feat(hero): create header and hero components

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,32 +2,43 @@ import React from "react";
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-gray-800 text-white py-4 px-6 flex justify-between items-center">
-      <nav className="flex space-x-6">
-        <a href="#home" className="hover:text-gray-400">
-          Início
-        </a>
-        <a href="#features" className="hover:text-gray-400">
-          Funcionalidades
-        </a>
-        <a href="#pricing" className="hover:text-gray-400">
-          Preços
-        </a>
-      </nav>
+    <header className="bg-transparent fixed top-0 text-foreground w-full center h-[80px]">
+      <div className="center !justify-between max-w-screen-2xl w-full">
+        <nav className="flex space-x-6 ">
+          <a
+            href="#home"
+            className="hover:border-b-foreground border border-solid border-transparent transition-all"
+          >
+            Início
+          </a>
+          <a
+            href="#features"
+            className="hover:border-b-foreground border border-solid border-transparent transition-all"
+          >
+            Funcionalidades
+          </a>
+          <a
+            href="#pricing"
+            className="hover:border-b-foreground border border-solid border-transparent transition-all"
+          >
+            Preços
+          </a>
+          <a
+            href="#pricing"
+            className="hover:border-b-foreground border border-solid border-transparent transition-all"
+          >
+            Cadastre-se
+          </a>
+        </nav>
 
-      <div className="flex space-x-4">
-        <a
-          href="#signup"
-          className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded"
-        >
-          Cadastre-se
-        </a>
-        <a
-          href="#login"
-          className="bg-gray-600 hover:bg-gray-700 text-white font-semibold py-2 px-4 rounded"
-        >
-          Login
-        </a>
+        <div className="flex space-x-4">
+          <a
+            href="#login"
+            className="main-gradient w-[120px] text-center text-white font-poppinsLight py-2 px-4 rounded"
+          >
+            Login
+          </a>
+        </div>
       </div>
     </header>
   );
--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,102 +1,39 @@
+// components/Hero.tsx
 "use client";
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { ReactTyped } from "react-typed";
+import LoginForm from "../form";
 
 const Hero: React.FC = () => {
-  const phrases = [
-    "políticos",
-    "assessores",
-    "candidatos",
-    "estratégicos",
-    "organizados",
-  ];
-  const [currentPhraseIndex, setCurrentPhraseIndex] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentPhraseIndex((prevIndex) => (prevIndex + 1) % phrases.length);
-    }, 2000);
-
-    return () => clearInterval(interval);
-  }, [phrases.length]);
-
   return (
-    <section className="flex items-center justify-between min-h-screen bg-gray-100 p-6">
-      <div className="w-full md:w-1/2 lg:w-2/5">
-        <h1 className="text-3xl md:text-5xl font-bold mb-4">
-          Um software online de gestão política
-        </h1>
-        <p className="text-xl mb-6">
-          Eleve seu desempenho eleitoral a um novo nível
-          <br />
-          Gestão estratégica para{" "}
-          <span className="font-bold">{phrases[currentPhraseIndex]}</span>
-        </p>
-      </div>
-      <div className="w-full md:w-1/2 lg:w-2/5 bg-white p-6 rounded-lg shadow-md">
-        <h2 className="text-2xl font-bold mb-4">Login</h2>
-        <form>
-          <div className="mb-4">
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-gray-700"
-            >
-              E-mail
-            </label>
-            <input
-              id="email"
-              type="email"
-              className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Senha
-            </label>
-            <input
-              id="password"
-              type="password"
-              className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50"
-              required
-            />
-          </div>
-          <div className="flex items-center justify-between mb-4">
-            <a
-              href="#forgot-password"
-              className="text-sm text-blue-500 hover:underline"
-            >
-              Esqueci minha senha
-            </a>
-          </div>
-          <button
-            type="submit"
-            className="w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          >
-            Entrar
-          </button>
-          <div className="my-4 text-center">
-            <p className="text-sm text-gray-600">ou entre com</p>
-            <div className="flex justify-center space-x-4 mt-2">
-              <button className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-                Google
-              </button>
-              <button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                Facebook
-              </button>
-            </div>
-          </div>
-          <div className="text-center mt-4">
-            <p className="text-sm text-gray-600">
-              Não tem uma conta?{" "}
-              <a href="#signup" className="text-blue-500 hover:underline">
-                Cadastre-se
-              </a>
-            </p>
-          </div>
-        </form>
+    <section className="center min-h-screen bg-background">
+      <div className="max-w-screen-2xl center !justify-between w-full">
+        <div className="">
+          <h1 className="text-[80px] title-gradient uppercase leading-[1] w-auto font-poppinsExtraBold font-bold mb-4">
+            Um software online <br /> de gestão política
+          </h1>
+          <p className="text-2xl mb-6">
+            Eleve seu desempenho eleitoral a um novo nível
+            <br />
+            Gestão estratégica para{" "}
+            <span className="font-bold">
+              <ReactTyped
+                strings={[
+                  "políticos",
+                  "assessores",
+                  "candidatos",
+                  "estratégicos",
+                  "organizados",
+                ]}
+                typeSpeed={50}
+                backSpeed={30}
+                backDelay={1000}
+                loop
+              />
+            </span>
+          </p>
+        </div>
+        <LoginForm />
       </div>
     </section>
   );
